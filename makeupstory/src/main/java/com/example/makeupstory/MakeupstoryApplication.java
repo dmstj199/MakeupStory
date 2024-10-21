74d1fff5d4b4740277a72442ea46492163610469
eunseo <dmstj199@naver.com>
2024-10-21 16:27:30 +0900

    Initial commit

---
+++ makeupstory/src/main/java/com/example/makeupstory/MakeupstoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.makeupstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeupstoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeupstoryApplication.class, args);
+	}
+
+}
---
+++ makeupstory/src/test/java/com/example/makeupstory/MakeupstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makeupstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeupstoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
